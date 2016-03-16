216f081ffe19cef910f9ab54d05b061e3696c5c7
vijay-developer <vijayse.1983@gmail.com>
2016-03-16 14:35:31 +0530

    Signed-off-by: vijay-developer <vijayse.1983@gmail.com>

---
+++ vTiger_CRM_extension/ext_content/app/controllers/index.js
@@ -0,0 +1,1 @@
+(function($,apploader,utils,props,$HELPER){var $app=window.vgrome;$app.controller('IndexCtrl',['$scope','$rootScope','$compile','apiProvider','lang','$controller',function($scope,$rootScope,$compile,$apiProvider,$lang,$controller){$.extend(this,$controller('BaseIndexCtrl',{$scope:$scope}));}]);})(jQuery,window.Apploader,window.UTILS,window.PROPS,window.VTEHelperInstance);
