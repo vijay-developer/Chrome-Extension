bacdcc36e92574a6059bc85a8d59adfae884a84a
vijay-developer <vijayse.1983@gmail.com>
2016-03-18 20:29:22 +0530

    UI Remove
    UI Remove

--- ui/common/properties.js
+++
@@ -1,9 +0,0 @@
-var PROPS = PROPS || {
-        debug: true,
-        version: 1,
-        sidebarWidth: 320,
-        //RB: 'https://netvill.dev/ui/',
-        //serviceBaseUrl: 'https://netvill.dev/api',
-        RB: 'https://174.136.15.141/forecast/ui/',
-        serviceBaseUrl: 'https://174.136.15.141/forecast/api'
-    };
